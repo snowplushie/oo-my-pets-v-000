4d5cd8537c6400e1eb57783b976e8ec9eaa0787e
Paige Wiener <snowplushie@yahoo.com>
2019-03-26 20:23:58 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -68,6 +68,6 @@ class Owner
   end
   
   def list_pets
-    "I have #{@pets[:fishes].count} fishes, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
   end
 end
