8b794cee784d821f819f716ec7d895851fb8a6c5
Paige Wiener <snowplushie@yahoo.com>
2019-03-26 20:23:46 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -66,4 +66,8 @@ class Owner
     @pets[:fishes].each {|f| f.mood = "nervous"}
     @pets.clear
   end
+  
+  def list_pets
+    "I have #{@pets[:fishes].count} fishes, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+  end
 end
