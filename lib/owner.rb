7c361488af8b97aef286a39db9d8b7069abd9d59
Paige Wiener <snowplushie@yahoo.com>
2019-03-26 20:14:16 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -16,4 +16,19 @@ class Owner
   def name
     @name
   end
+  
+  def buy_cat (name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
+  
+  def buy_dog (name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
+  end
+  
+  def buy_fish (name)
+    fish = Fish.new(name)
+    @pets[:fishes] << fish
+  end
 end
