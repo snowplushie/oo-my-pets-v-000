1564f0793e8cda586650a808c51cf0a65efa93bd
Paige Wiener <snowplushie@yahoo.com>
2019-03-26 20:18:58 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -47,4 +47,16 @@ class Owner
   def say_species
     "I am a #{@species}."
   end
+  
+  def walk_dogs
+    @pets[:dogs].each do {|d| d.mood = "happy"}
+  end
+  
+  def play_with_cats
+    @pets[:cats].each do {|c| c.mood = "happy"}
+  end
+  
+  def feed_fish
+    @pets[:fishes].each do {|f| f.mood = "happy"}
+  end
 end
